773dbe9e39b1a5a429676159bf03c7672e63a66f
Dimitris Trachiotis <acemod13@outlook.com>
2018-12-02 15:45:38 +0200

    Quick Bugfix.

--- CompilerCore/CoreCode.cs
+++ CompilerCore/CoreCode.cs
@@ -95,7 +95,7 @@ namespace CompilerCore
         public static void RunTest(string sdkLocation, string projectLocation)
         {
             if (File.Exists(Path.Combine(sdkLocation, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "nwjs.exe" : "nwjs")))
-                Process.Start(Path.Combine(sdkLocation, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "nwjs.exe": "nwjs"), "--nwapp=\"file://" + projectLocation + "\"");
+                Process.Start(Path.Combine(sdkLocation, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "nwjs.exe": "nwjs"), "--nwapp=\"" + projectLocation + "\"");
             else if (File.Exists(Path.Combine(sdkLocation, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Game.exe" : "Game")))
                 Process.Start(Path.Combine(sdkLocation, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Game.exe": "Game"),
                     "--nwapp=\"" + projectLocation + "\"");
