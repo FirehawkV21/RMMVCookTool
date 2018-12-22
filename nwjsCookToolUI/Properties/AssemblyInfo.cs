4effe98eab99a6e537ebf5507d0db26731f583b4
Dimitris Trachiotis <acemod13@outlook.com>
2018-12-22 20:57:34 +0200

    Minor correction to the version text.

--- nwjsCookToolUI/Properties/AssemblyInfo.cs
+++ nwjsCookToolUI/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.18338")]
-[assembly: AssemblyFileVersion("1.4.0.18338")]
+[assembly: AssemblyVersion("1.4.0.18356")]
+[assembly: AssemblyFileVersion("1.4.0.18356")]
