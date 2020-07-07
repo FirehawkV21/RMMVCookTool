6821065228a539fcc16cc87d8780a1e1b0bd8928
Dimitris Trachiotis <acemod13@outlook.com>
2020-07-07 18:00:40 +0300

    Initial design for R3.

---
+++ RMMVCookTool.GUI/ProjectSettingsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RMMVCookTool.GUI
+{
+    /// <summary>
+    /// Interaction logic for ProjectSettingsWindow.xaml
+    /// </summary>
+    public partial class ProjectSettingsWindow : Window
+    {
+        public ProjectSettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
