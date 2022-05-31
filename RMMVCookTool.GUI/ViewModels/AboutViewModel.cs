a6fb3b8ceb280fa1d48740b4f02b7a857d1f7c54
Dimitris Trachiotis <acemod13@outlook.com>
2022-05-31 18:11:51 +0300

    Implement an alternate way for the manual.

--- RMMVCookTool.GUI/ViewModels/AboutViewModel.cs
+++ RMMVCookTool.GUI/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -6,16 +7,39 @@ namespace RMMVCookTool.GUI.ViewModels;
 public class AboutViewModel : BindableBase
 {
     private string programVersion = "";
+    private bool docsAvailable = true;
     public string ProgramVersionText {
         get => programVersion;
         set => SetProperty(ref programVersion, value);
     }
+    public bool AreDocsAvailable { get => docsAvailable; set => SetProperty(ref docsAvailable, value); }
+    public DelegateCommand OpenDocsCommand { get; private set; }
+    private static readonly string ReadmeFile =
+    Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\Docs\\Manual.pdf";
+
+    private static readonly string GreekReadme =
+        Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\Docs\\Manual.el.pdf";
 
     public AboutViewModel()
     {
+        OpenDocsCommand = new DelegateCommand(OpenReadme, CheckFile).ObservesProperty(() => AreDocsAvailable);
         Assembly assembly = Assembly.GetExecutingAssembly();
         FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
         string version = fvi.FileVersion;
         ProgramVersionText = Resources.ProgramVersionLabelUiText + @" (" + version + @")";
+        string docsFile = (System.Threading.Thread.CurrentThread.CurrentCulture.Name == "el-GR") ? GreekReadme : ReadmeFile;
+        if (!File.Exists(docsFile)) AreDocsAvailable = false;
+    }
+    
+    private bool CheckFile()
+    {
+        return AreDocsAvailable;
+    }
+
+    private void OpenReadme() {
+        using Process fileopener = new Process();
+        fileopener.StartInfo.FileName = "explorer";
+        fileopener.StartInfo.Arguments = "\"" + Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString(), "Docs") + "\"";
+        fileopener.Start();
     }
 }
